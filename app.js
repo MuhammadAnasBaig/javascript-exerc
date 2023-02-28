20b8249afcc0079a77dd29e6f2d2872298e54050
muhammadanasbaig <muhammmadanasasif3@gmail.com>
2023-02-28 15:38:12 +0500

    17 - 20

---
+++ app.js
@@ -0,0 +1,151 @@
+/// Q)1
+
+// const myArray = [
+     
+//     [],
+//     [],
+//     []
+// ]
+
+
+////       Q)2
+
+// var matrix = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+
+// ]
+// document.write(matrix[0] +"<br>" + matrix[1] + "<br>" + matrix[2]);
+
+
+////  Q)3
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(i);
+//   }
+  
+  ///   Q)4
+
+// let number = prompt("Enter a number to show it's multiplication table ")
+// let length = prompt("Enter length of the table")
+// console.log(`Table of ${number} of length is:`)
+// for (let i=1; i<=length; i++){
+//     console.log(`${number} * ${i} = ${number * i}`);
+// }
+
+// Q)5
+
+// let fruits =[
+      
+//     ["apple"],
+//     ["banana"],
+//     ["mango"],
+//     ["orange"],
+//     ["strawberry"]
+// ]
+// console.log(
+//     `Element at index 0 is ${fruits [0]}\n`,
+//     `Element at index 1 is ${fruits [1]}\n`,
+//     `Element at index 2 is ${fruits [2]}\n`,
+//     `Element at index 3 is ${fruits [3]}\n`,
+//     `Element at index 4 is ${fruits [4]}\n`
+// )
+
+//     Q)6
+
+            // part A 
+
+//   document.write("<h4>Counting:</h4>")
+//    for (let i=1; i<=15;i++){
+//     document.write(i + ", ")
+//   }
+  
+//   ///////   part B
+
+//   document.write("<h4> Reverse Counting:</h4>")
+//   for (let i=10; i>=1;i--){
+//    document.write(i + ", ")
+//  }
+
+// //////// part C
+
+
+// document.write("<h4>Even:</h4>")
+// for (let i=0; i<=20;i+=2){
+//  document.write(i + " ")
+// }
+  
+// /// part  D
+
+// document.write("<h4>Odd:</h4>")
+// for (let i=1; i<=19;i+=1){
+//  document.write(i + " ")
+// }
+  
+
+// ///// part E
+
+// document.write("<h4>Series:</h4>")
+// for (let i=2; i<=20;i+=2){
+//  document.write(i +"k" +" ,")
+// }
+
+
+/// Q)7
+
+
+// var a = ["cake","apple pie","cookie","chips","patties",]
+//      var user = prompt("Welcome to My bakery whats do you want to order?")
+//      var flags = false
+//      for(var i = 0;i < a.length;i++){
+//         if(a[i] === user){
+//              document.write(`${a[i]} is available at index ${i} in our bakery`)
+//          flags = true
+//              break
+//          }
+
+//      }
+//      if(flags == false){
+//          document.write(`we are sorry ${user} is not available in our bakery`)
+//      }
+
+  // Q)8
+
+// var a = [24,53,78,91,12]
+// var store = 0;
+// document.write(`Array items: ${a}`)
+// for(var i=0;i<=a.length;i++){
+//   if(a[i]>store){
+//      store = a[i]
+//   }
+// }
+  
+// document.write(`The largest number: ${store}`)
+
+
+/// Q)9
+
+// var b = [24,53,78,91,12]
+// var stores = b[0];
+// document.write(`Array items: ${b}`)
+// for(var i=0;i<=b.length;i++){
+//   if(stores > b[i]){
+//      stores = b[i]
+//   }
+// }
+  
+// document.write(`The smallest number: ${stores}`)
+
+// Q)10
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+
+////////////                   end              /////
+
+
+
